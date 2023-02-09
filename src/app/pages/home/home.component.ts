040b022b4b8995bb7257a5a3beed321b4933e59e
Roshane <39389600+Roshane-Johnson@users.noreply.github.com>
2023-02-09 16:55:26 -0500

    second commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiResponse } from 'src/app/interfaces/api-response';
+import { User } from 'src/app/interfaces/user';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private userService = inject(UserService);
+  users: User[] = [];
+  constructor() {
+    this.userService.getAll().subscribe({
+      next: (resp: ApiResponse<User[]>) => {
+        this.users = resp.data;
+      },
+    });
+  }
 
+  ngOnInit(): void {}
 }
